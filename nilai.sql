5172852c587054e9b92bd2ae905c9f84fd40056f
Aulia-23 <75212157+Aulia-23@users.noreply.github.com>
2023-07-11 15:40:14 +0700

    Add files via upload

---
+++ nilai.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2023 at 10:16 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `technical-test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `nim` int(11) NOT NULL,
+  `kode_mk` varchar(11) NOT NULL,
+  `nama_mk` varchar(20) NOT NULL,
+  `sks` int(3) NOT NULL,
+  `nilai_angka` int(3) NOT NULL,
+  `nilai_huruf` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nilai`
+--
+
+INSERT INTO `nilai` (`kode_mk`, `nama_mk`, `Jumlah Mahasiswa yang Mendapat Nilai A atau B`, `Nilai Rata-rata pada Mahasiswa yang Berumur lebih dari 20 dan kurang dari 22`) VALUES
+('MKA', 'Matematika', '1', '77.5'),
+('MKB', 'Biologi', '1', '59.0'),
+('MKC', 'Fisika', '0', '20.0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD KEY `nim` (`nim`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
